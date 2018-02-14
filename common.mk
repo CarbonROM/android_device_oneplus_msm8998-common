a01bcc3c6e55de37ca22fc4954a5ccb92b6553c4
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-14 20:42:13 +0100

    msm8998: add perfboostsconfig and powerhint configs from OOS b5
    Change-Id: I0909aec75a3501a296151fc6ad9916de94e1706a

--- common.mk
+++ common.mk
@@ -333,6 +333,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.0-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
