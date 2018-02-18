d63f3789ad80707035c1103b413e6dd877459c91
Brett Montague <bmontagu@gmail.com>
2018-02-17 22:47:37 -0500

    msm8998: add back qti powerhal
    partially reverts commit 27e707b4ec9978311b937d7e2444c0c2e89ae220
    
    Signed-off-by: Brett Montague <bmontagu@gmail.com>

--- common.mk
+++ common.mk
@@ -329,9 +329,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.msm8998 \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
