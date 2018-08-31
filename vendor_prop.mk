7d2659fe8459288665c4913c60e456e7fb4355cd
neobuddy89 <neobuddy89@gmail.com>
2018-09-01 05:13:22 +0530

    msm8998: Add extra props for VoLTE
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>
    Change-Id: I9bc7750d76a75cb921f027878685614d8fc8c330

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -290,6 +290,27 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.vndk.version=28
 
+# VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.ims.dropset_feature=0 \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.radio.force_on_dc=true \
+    persist.radio.custom_ecc=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.ignore_dom_time=10 \
+    persist.radio.rat_on=combine \
+    persist.radio.sib16_support=1 \
+    persist.radio.RATE_ADAPT_ENABLE=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.is_wps_enabled=true \
+    persist.radio.videopause.mode=1 \
+    persist.radio.sap_silent_pin=1 \
+    persist.radio.always_send_plmn=true \
+    persist.rcs.supported=0
+
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
