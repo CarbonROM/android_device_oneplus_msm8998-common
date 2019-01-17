9decbf213d62da0a5f4b95ef23aa1807cc2d8c50
neobuddy89 <neobuddy89@gmail.com>
2019-01-17 19:16:59 +0530

    msm8998: Add prop to enable vendor ims service
    Change-Id: I75750125909535134bea82c60a0467180c10af19
    Signed-off-by: Mrinal Kant Shukla <kamikaze.mrinal@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -250,7 +250,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
-    persist.radio.calls.on.ims=1
+    persist.radio.calls.on.ims=1 \
+    vendor.service.qti.ims.enabled=1
 
 # RmNet Data
 PRODUCT_PROPERTY_OVERRIDES += \
