17d798f17791118b5f270c14c0ebe2335c9b3d66
kazerine <kamikaze.mrinal@gmail.com>
2019-06-02 13:39:21 +0530

    msm8998-common: whatsapp use hal1
    Signed-off-by: Mrinal Kant Shukla <kamikaze.mrinal@gmail.com>
    Change-Id: I12592301acc3b895804d5f7a330e45e07a8d7d95

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -86,6 +86,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.oneplus.camera,com.android.camera,org.lineageos.snap \
+    camera.hal1.packagelist=com.whatsapp \
     persist.camera.mobicat=2 \
     persist.camera.stats.debugexif=3080192 \
     persist.ts.rtmakeup=false \
